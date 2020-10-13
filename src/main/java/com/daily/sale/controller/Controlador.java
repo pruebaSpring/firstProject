25f454d208d8f102c2abd656c4e2aa525ade355c
Carlos Pino <cpino@efact.pe>
2020-10-12 20:32:35 -0500

    Add controller

--- src/main/java/com/daily/sale/controller/Controlador.java
+++ src/main/java/com/daily/sale/controller/Controlador.java
@@ -27,60 +27,88 @@ public class Controlador {
 	private IPersonaService service;
 	
 	@GetMapping("/index")
-	public String listar(Model model) {
+	public String index(Model model) {
 		List<Persona> personas = service.listar();
 		model.addAttribute("personas", personas);
 		return "index";
 	}
 	
-	@GetMapping("/about")
-	public String about(Model model) {
+	@GetMapping("/reservados")
+	public String reservados(Model model) {
 		List<Persona> personas = service.listar();
 		model.addAttribute("personas", personas);
-		return "about";
+		return "reservados";
 	}
 
-	@GetMapping("/gallery")
-	public String gallery(Model model) {
+	@GetMapping("/vendidos")
+	public String vendidos(Model model) {
 		List<Persona> personas = service.listar();
 		model.addAttribute("personas", personas);
-		return "gallery";
-	}
-
-	@GetMapping("/blog")
-	public String blog(Model model) {
-		List<Persona> personas = service.listar();
-		model.addAttribute("personas", personas);
-		return "blog";
-	}
-
-	@GetMapping("/contact")
-	public String contact(Model model) {
-		List<Persona> personas = service.listar();
-		model.addAttribute("personas", personas);
-		return "contact";
+		return "vendidos";
 	}
 	
-	@GetMapping("/insertar")
-	public String insert(Model model) {
-		MongoClientURI uri = new MongoClientURI("mongodb+srv://admin:aqafdhb@clusterspringexample.q1vcv.mongodb.net/dbTEST?retryWrites=true&w=majority");
-        MongoClient mongo = new MongoClient(uri);
-        MongoDatabase mongoDatabase = mongo.getDatabase("dbTEST");
-        MongoCollection<Document> collection = mongoDatabase.getCollection("TRANSACTION");
-        System.out.println(collection);
-        System.out.println("Connect to database successfully");  
-        
-        System.out.println("Inserción de documento");
-        
-        Document document = new Document("name", "Luis").
-          append("email", "lolaya@efact.pe").
-          append("twitter", "lolaya@twitt.pe").
-          append("location", new Document("city", "Villa Salvaje").append("zip", 12345));
-        
-        collection.insertOne(document);
-        
-        System.out.println("Documento agregado");
-        
-        return "index";
+	@GetMapping("/disponibles")
+	public String disponibles(Model model) {
+		List<Persona> personas = service.listar();
+		model.addAttribute("personas", personas);
+		return "disponibles";
 	}
+	
+	@GetMapping("/colors")
+	public String colors(Model model) {
+		List<Persona> personas = service.listar();
+		model.addAttribute("personas", personas);
+		return "colors";
+	}
+	
+//	@GetMapping("/about")
+//	public String about(Model model) {
+//		List<Persona> personas = service.listar();
+//		model.addAttribute("personas", personas);
+//		return "about";
+//	}
+//
+//	@GetMapping("/gallery")
+//	public String gallery(Model model) {
+//		List<Persona> personas = service.listar();
+//		model.addAttribute("personas", personas);
+//		return "gallery";
+//	}
+//
+//	@GetMapping("/blog")
+//	public String blog(Model model) {
+//		List<Persona> personas = service.listar();
+//		model.addAttribute("personas", personas);
+//		return "blog";
+//	}
+//
+//	@GetMapping("/contact")
+//	public String contact(Model model) {
+//		List<Persona> personas = service.listar();
+//		model.addAttribute("personas", personas);
+//		return "contact";
+//	}
+//	
+//	@GetMapping("/insertar")
+//	public String insert(Model model) {
+//		MongoClientURI uri = new MongoClientURI("mongodb+srv://admin:aqafdhb@clusterspringexample.q1vcv.mongodb.net/dbTEST?retryWrites=true&w=majority");
+//        MongoClient mongo = new MongoClient(uri);
+//        MongoDatabase mongoDatabase = mongo.getDatabase("dbTEST");
+//        MongoCollection<Document> collection = mongoDatabase.getCollection("TRANSACTION");
+//        System.out.println(collection);
+//        System.out.println("Connect to database successfully");  
+//        
+//        System.out.println("Inserción de documento");
+//        
+//        Document document = new Document("name", "Luis").
+//          append("email", "lolaya@efact.pe").
+//          append("twitter", "lolaya@twitt.pe").
+//          append("location", new Document("city", "Villa Salvaje").append("zip", 12345));
+//        
+//        collection.insertOne(document);
+//        
+//        System.out.println("Documento agregado");
+//        
+//        return "index";
+//	}
 }
